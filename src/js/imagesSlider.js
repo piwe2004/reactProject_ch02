6b0faa0c4a501952e0902a5f4bad3d616c7e7dc2
piwe2004 <piwe2004@gmail.com>
2022-05-11 14:35:09 +0900

    이미지슬라이드 프로젝트

--- src/js/imagesSlider.js
+++ src/js/imagesSlider.js
@@ -2,16 +2,23 @@ export default class ImagesSlider {
     #currentPosition = 0;
     #slideNumber = 0;
     #slideWidth = 0;
+    #intervalId;
+    #autoPlay = true;
     sliderWrapEl;
     sliderListEl;
     nextBtnEl;
-    prevBtnEl
+    prevBtnEl;
+    indicatorWrapEl;
+    controlWrapEl;
     constructor() {
         this.assignElement();
         this.initSliderNumber();
         this.initSliderWidth();
         this.initSliderListWidth()
         this.addEvent();
+        this.createIndicator();
+        this.setIndicator();
+        this.initAutoPlay();
     }
 
     assignElement (){
@@ -19,6 +26,12 @@ export default class ImagesSlider {
         this.sliderListEl = this.sliderWrapEl.querySelector('#slider');
         this.nextBtnEl = this.sliderWrapEl.querySelector('#next');
         this.prevBtnEl = this.sliderWrapEl.querySelector('#previous');
+        this.indicatorWrapEl = this.sliderWrapEl.querySelector('#indicator-wrap');
+        this.controlWrapEl = this.sliderWrapEl.querySelector('#control-wrap');
+    }
+
+    initAutoPlay (){
+        this.#intervalId = setInterval(this.moveToRight.bind(this), 3000);
     }
 
     initSliderNumber(){
@@ -34,16 +47,50 @@ export default class ImagesSlider {
     }
 
     addEvent(){
-        this.nextBtnEl.addEventListener('click', this.movoToRight.bind(this))
-        this.prevBtnEl.addEventListener('click', this.moveToLeft.bind(this))
+        this.nextBtnEl.addEventListener('click', this.moveToRight.bind(this));
+        this.prevBtnEl.addEventListener('click', this.moveToLeft.bind(this));
+        this.indicatorWrapEl.addEventListener('click', this.onClickIndicator.bind(this));
+        this.controlWrapEl.addEventListener('click', this.togglePlay.bind(this));
     }
 
-    movoToRight(){
+    togglePlay(e){
+        if(e.target.dataset.status === 'play'){
+            this.#autoPlay = true;
+            this.controlWrapEl.classList.add('play');
+            this.controlWrapEl.classList.remove('pause');
+            this.initAutoPlay();
+        }else if(e.target.dataset.status === 'pause'){
+            this.#autoPlay = false;
+            this.controlWrapEl.classList.add('pause');
+            this.controlWrapEl.classList.remove('play');
+            clearInterval(this.#intervalId);
+        }
+    }
+
+    onClickIndicator(e){
+        const indexPosition = parseInt(e.target.dataset.index, 10);
+        if(Number.isInteger(indexPosition)){
+            this.#currentPosition = indexPosition;
+            this.sliderListEl.style.left = `-${this.#slideWidth * this.#currentPosition}px`;
+            if (this.#autoPlay){
+                clearInterval(this.#intervalId);
+                this.#intervalId = setInterval(this.moveToRight.bind(this), 3000);
+            }
+            this.setIndicator();
+        }
+    }
+
+    moveToRight(){
         this.#currentPosition +=1;
         if(this.#currentPosition === this.#slideNumber){
             this.#currentPosition = 0;
         }
         this.sliderListEl.style.left = `-${this.#slideWidth * this.#currentPosition}px`;
+        if (this.#autoPlay){
+            clearInterval(this.#intervalId);
+            this.#intervalId = setInterval(this.moveToRight.bind(this), 3000);
+        }
+        this.setIndicator();
     }
 
     moveToLeft(){
@@ -52,5 +99,25 @@ export default class ImagesSlider {
             this.#currentPosition = this.#slideNumber-1;
         }
         this.sliderListEl.style.left = `-${this.#slideWidth * this.#currentPosition}px`;
+        if (this.#autoPlay){
+            clearInterval(this.#intervalId);
+            this.#intervalId = setInterval(this.moveToRight.bind(this), 3000);
+        }
+        this.setIndicator();
+    }
+
+    createIndicator(){
+        const docFragment = document.createDocumentFragment();
+        for (let i =0; i < this.#slideNumber; i++){
+            const li = document.createElement('li');
+            li.dataset.index = i;
+            docFragment.appendChild(li);
+        }
+        this.indicatorWrapEl.querySelector('ul').appendChild(docFragment);
+    }
+
+    setIndicator(){
+        this.indicatorWrapEl.querySelector('li.active')?.classList.remove('active');
+        this.indicatorWrapEl.querySelector(`ul li:nth-child(${this.#currentPosition + 1})`).classList.add('active');
     }
 }
